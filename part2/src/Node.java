00509e72f6b1996a585c53cfcf2a5ff9abf1f14a
kvartborg <hello@kvartborg.net>
2019-04-10 15:32:54 +0200

    Add comment to node

--- part2/src/Node.java
+++ part2/src/Node.java
@@ -1,14 +1,24 @@
 public class Node {
-
+  /**
+   * Left child
+   */
   public Node left;
+
+  /**
+   * Rigth child
+   */
   public Node right;
 
+  /**
+   * Key contains the given integer value
+   */
   public int key;
 
+  /**
+   * Construct a new node by giving it a key
+   * @param key is the given integer
+   */
   public Node(int key) {
     this.key = key;
   }
-
-
-
 }
